0872954fd944782aab190d9ca757b02f50c2a9c0
Joren <Joren@Joren-PC>
2018-09-27 10:10:28 +0200

    address service changes (removed comments and added 2 new methods)

--- ClientRepository/src/main/java/com/realdolmen/maven/clientrepository/services/AddressService.java
+++ ClientRepository/src/main/java/com/realdolmen/maven/clientrepository/services/AddressService.java
@@ -29,50 +29,34 @@ public class AddressService {
         this.personService = personService;
         this.postalCodeService = postalCodeService;
     }
-    //Insert Address
 
     public Address insertAddress(Address address) throws NoQueryPossibleException {
         int i = addressRepository.insertItem(address);
         return findAddressById(i);
     }
-
-    //address -> reporsirtoy.findbyid
-    //addres.getPostalCode.getPostalCode()
-    //postalCodeService.findById(address.getPostalCode.getPostalCode())
-    //address.setPostalCode(PostalCode)
-    //return address  
+  
     public Address findAddressById(int id) throws NoQueryPossibleException {
         Address address = addressRepository.findById(id);
         address.setPostalCode(postalCodeService.findById(address.getPostalCode().getNumber()));
         return address;
     }
-    //findAllAddressForClientPerson(int id)
-    //zoek een adres die van een bepaald persoon    
-    //findAllAddressFromClientFirm(int id)
 
-//findAllAddressForClientPerson(int id)
     public List<Address> findAllAddressForClientPerson(int id) throws NoQueryPossibleException {
-        /* List<Address> addresses = new ArrayList<>();
-        List<Address> allAddresses = addressRepository.findAll();
-        for (Address a : allAddresses) {
-            addresses.add(a);
-        }
-        return addresses;
-        ^waarschijnlijk fout^
-        
-        nogni juist, je moet address en person nog linken, hoe is de vraag*/
         return personService.findById(id).getAddress();
 
     }
-    //zoek een adres die van een bepaald persoon
-
-    //findAllAddressFromClientFirm(int id)
     public ArrayList<Address> findAllAddressFromClientFirm(int id) throws NoQueryPossibleException {
         return firmService.findById(id).getAddress();
 
     }
 
-    //findAddressesForPerson(int id)
+    public List<Address> findAddressForPerson(int id) throws NoQueryPossibleException{
+        return addressRepository.findAddressForPersonId(id);
+    }
 
-    //findAddressesForFirm(int id)
+    public List<Address> findAddressForFirm(int id){
+        //find address for firm nog in repo maken
+        //return addressRepository.findAddressForFirm(id);
+        return null;
+    }
 }
